7933029c79de6ac3bb8febeb6bb47a707e504394
Eric Drury <edrury@users.noreply.github.com>
2017-09-08 13:35:04 -0500

    use col-md-12 for simple page template

--- functions.php
+++ functions.php
@@ -6,3 +6,13 @@ add_action( 'after_setup_theme', 'sparkling_child_setup', 20 );
 function sparkling_child_setup() {
 	remove_action( 'wp_head', 'get_sparkling_theme_options', 10 );
 }
+
+function sparkling_main_content_bootstrap_classes() {
+	if ( is_page_template( 'page-fullwidth.php' ) ) {
+		return 'col-sm-12 col-md-12';
+	}
+	if ( is_page_template( 'simple-page.php' ) ) {
+		return 'col-sm-12 col-md-12';
+	}
+	return 'col-sm-12 col-md-8';
+}
