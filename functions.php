b32c2ed992f5f1cdace43d771446c76e6f306139
Eric Drury <edrury@users.noreply.github.com>
2017-09-09 17:58:30 -0500

    remove comments from attachments and galleries https://jetpack.com/2013/05/01/remove-comments-carousel/

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ add_action( 'after_setup_theme', 'sparkling_child_setup', 20 );
 
 function sparkling_child_setup() {
 	remove_action( 'wp_head', 'get_sparkling_theme_options', 10 );
+	add_filter( 'comments_open', 'tweakjp_rm_comments_att', 10 , 2 );
 }
 
 function sparkling_main_content_bootstrap_classes() {
@@ -15,4 +16,12 @@ function sparkling_main_content_bootstrap_classes() {
 		return 'col-sm-12 col-md-12';
 	}
 	return 'col-sm-12 col-md-8';
+}
+
+function tweakjp_rm_comments_att( $open, $post_id ) {
+    $post = get_post( $post_id );
+    if( $post->post_type == 'attachment' ) {
+        return false;
+    }
+    return $open;
 }
